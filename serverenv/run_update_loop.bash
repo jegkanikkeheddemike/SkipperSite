5626976dafee09ac5d30dfe7a37cde1cb09dc1b7
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2022-01-29 11:50:26 +0100

    Create run_update_loop.bash

---
+++ serverenv/run_update_loop.bash
@@ -0,0 +1,9 @@
+looping=true
+#Will keep looping until serverenv exits with an error.
+#serverenv will exit with an error if it decides it is time to end the loop
+#and shut down
+while $looping; do
+    git reset --hard
+    git pull
+    cargo run --bin serverenv --release || $looping = false
+done
