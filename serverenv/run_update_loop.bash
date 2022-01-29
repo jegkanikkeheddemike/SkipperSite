fd823f84b4381fa4f00ba1d3f269c30be51dcff0
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2022-01-29 12:30:42 +0100

    Update run_update_loop.bash

--- serverenv/run_update_loop.bash
+++ serverenv/run_update_loop.bash
@@ -3,7 +3,7 @@ looping=true
 #serverenv will exit with an error if it decides it is time to end the loop
 #and shut down
 while $looping; do
-    #git reset --hard
-    #git pull
+    git reset --hard
+    git pull
     /home/$USER/.cargo/bin/cargo run --bin serverenv --release || looping=false
 done
