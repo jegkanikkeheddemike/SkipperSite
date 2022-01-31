50183b395368de9004efa0300eb1e0ec027690f8
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2022-01-31 13:38:09 +0100

    work on chat

--- serverenv/server/public/static/chat.js
+++ serverenv/server/public/static/chat.js
@@ -1,8 +1,13 @@
 let messages_url = window.location.href + "/api/messages";
+console.log(messages_url);
 var messages;
 $.getJSON(messages_url, (data) => {
     messages = data;
+
+    console.log("LENGTH: " + messages.length);
+
     messages.forEach(element => {
+        console.log("LOG")
         console.log(element.message_content);
     });
 });
