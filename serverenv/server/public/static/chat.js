7ce852321df4e78d13d346074983f0f43d6527c1
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2022-01-31 15:46:32 +0100

    chat app message sending and loading

--- serverenv/server/public/static/chat.js
+++ serverenv/server/public/static/chat.js
@@ -1,14 +1,56 @@
-let messages_url = window.location.href + "/api/messages";
-console.log(messages_url);
-var messages;
-$.getJSON(messages_url, (data) => {
-    messages = data;
+var messages_url = window.location.href + "/api/messages";
 
-    console.log("LENGTH: " + messages.length);
-
-    messages.forEach(element => {
-        console.log("LOG")
-        console.log(element.message_content);
-    });
+var app = new Vue({
+    el: '#app',
+    data: {
+        messages: [],
+        chat_name: "No chat loaded",
+        uid: 0
+    }
 });
 
+function update_messages() {
+    $.getJSON(messages_url, (data) => {
+
+        app.messages = [];
+
+        data.forEach(element => {
+            app.messages.push(element);
+        });
+    });
+}
+update_messages();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function update_loop() {
+    while (true) {
+        await sleep(1000);
+        update_messages();
+    }
+}
+
+update_loop();
+
+function post_message() {
+    let formdata = JSON.stringify({
+        userid: 0,
+        message_content: chat_input_field.value
+    });
+
+    console.log(formdata);
+
+    $.ajax(
+        {
+            type: "POST",
+            url: messages_url,
+            data: formdata,
+            dataType: "json",
+            contentType: "application/json"
+        }
+    );
+    chat_input_field.value = "";
+    update_messages();
+}
